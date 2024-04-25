e2eb3819bf0d215cddf2506e5f9f8e69676b9450
iverly <github@iverly.net>
2024-04-25 19:10:19 +0200

    feat(terraform): add flux install module
    Signed-off-by: iverly <github@iverly.net>

---
+++ terraform/modules/kube-flux-install/main.tf
@@ -0,0 +1,10 @@
+resource "helm_release" "this" {
+  name = "flux2"
+
+  repository = "https://fluxcd-community.github.io/helm-charts"
+  chart      = "flux2"
+  version    = "2.12.4"
+
+  namespace = "flux-system"
+  wait      = true
+}
